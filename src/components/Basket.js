536f53d7e76c965c3ccb40d4f04e1752c23b664d
gark6654 <gark.6654@gmail.com>
2020-07-21 02:26:24 +0400

    Header part is ready

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -6,6 +6,7 @@ function Basket(props) {
         <div className="BasketBox col-md col-sm col-xs">
             <button className="BasketButton float-right">
                 <img className="BasketIcon" src={Icon} alt="basket" width="50" />
+                <label>My Cart</label>
                 <div className="BasketItemCount">
                     <span>0</span>
                 </div>
