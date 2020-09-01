37afed2b54d7207fc6b3f38727888514507297af
gark6654 <gark.6654@gmail.com>
2020-09-01 13:22:47 +0400

    Working in admin panel

--- front-end/src/components/LangSelector.js
+++ front-end/src/components/LangSelector.js
@@ -4,12 +4,14 @@ import { changeLang } from '../App';
 function LangSelector(props) {
     const changeLanguage = useContext(changeLang); // Function called from App component.
 
+    const selectedLanguage = localStorage.getItem('lang') || 'ENG';
+
     function change(event) {
         changeLanguage(event.target.value);
     }
 
     return (
-        <select className="LangSelector" onChange={change} value={localStorage.getItem('lang')}>
+        <select className="LangSelector" onChange={change} value={selectedLanguage}>
             <option>ENG</option>
             <option>РУС</option>
             <option>ՀԱՅ</option>
