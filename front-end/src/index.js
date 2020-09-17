f1cc511e4cb06f311afada898c2c111ebd676e00
gark6654 <gark.6654@gmail.com>
2020-09-17 12:17:03 +0400

    Fixed bug

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // Bootstrap styles.
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+// Custom styles.
 import App from './App';
 
 ReactDOM.render(
